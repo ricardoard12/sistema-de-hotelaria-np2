7dc769f6286474e50159a629c892bf70c4ea6333
nmacambira@gmail.com <nmacambira@gmail.com@3f7ef460-20b4-9d42-8ccf-13c00f1c48c6>
2013-06-01 22:43:40 +0000

    Todos os DAOs estão feitos.

--- Sistema de Hotelaria/src/hotel/persistencia/AluguelDAO.java
+++ Sistema de Hotelaria/src/hotel/persistencia/AluguelDAO.java
@@ -1,5 +1,72 @@
 package hotel.persistencia;
 
-public class AluguelDAO {
+import hotel.entidades.Aluguel;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+public class AluguelDAO {
+		public void inserir(Aluguel a) {
+			EntityManager em = JPAResourceBean.getEntityManager();
+			
+			try {
+				em.getTransaction().begin();
+				em.persist(a);
+				em.getTransaction().commit();
+			} catch(RuntimeException e) {
+				em.getTransaction().rollback();
+			} finally {
+				em.close();
+			}
+		}
+		
+		public void atualizar(Aluguel a) {
+			EntityManager em = JPAResourceBean.getEntityManager();
+			
+			try {
+				em.getTransaction().begin();
+				
+				Aluguel aVelho = em.find(Aluguel.class, a.getIdAluguel());
+				aVelho.setQtdPessoas(a.getQtdPessoas());
+				aVelho.setDataEntrada(a.getDataEntrada());
+				aVelho.setDataSaida(a.getDataSaida());
+				aVelho.setUsuario(a.getUsuario());
+				aVelho.setQuarto(a.getQuarto());
+				
+				em.getTransaction().commit();
+			} catch(RuntimeException e) {
+				em.getTransaction().rollback();
+			} finally {
+				em.close();
+			}
+		}
+		
+		public void excluir(int codigo) {
+			EntityManager em = JPAResourceBean.getEntityManager();
+			
+			try {
+				em.getTransaction().begin();
+				
+				Aluguel a = em.find(Aluguel.class, codigo);
+				em.remove(a);
+				
+				em.getTransaction().commit();
+			} catch(RuntimeException e) {
+				em.getTransaction().rollback();
+			} finally {
+				em.close();
+			}
+		}
+		
+		public Aluguel buscar(int codigo) {
+			EntityManager em = JPAResourceBean.getEntityManager();
+			return em.find(Aluguel.class, codigo);
+		}
+		
+		public List<Aluguel> listar() {
+			EntityManager em = JPAResourceBean.getEntityManager();
+			
+			return em.createQuery("select a from Aluguel a").getResultList();
+		}
 }
--- Sistema de Hotelaria/src/hotel/persistencia/ProdutoDAO.java
+++ Sistema de Hotelaria/src/hotel/persistencia/ProdutoDAO.java
@@ -1,5 +1,69 @@
 package hotel.persistencia;
 
-public class ProdutoDAO {
+import hotel.entidades.Produto;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+public class ProdutoDAO {
+			public void inserir(Produto p) {
+			EntityManager em = JPAResourceBean.getEntityManager();
+			
+			try {
+				em.getTransaction().begin();
+				em.persist(p);
+				em.getTransaction().commit();
+			} catch(RuntimeException e) {
+				em.getTransaction().rollback();
+			} finally {
+				em.close();
+			}
+		}
+		
+		public void atualizar(Produto p) {
+			EntityManager em = JPAResourceBean.getEntityManager();
+			
+			try {
+				em.getTransaction().begin();
+				
+				Produto pVelho = em.find(Produto.class, p.getIdProduto());
+				pVelho.setNome(p.getNome());
+				pVelho.setValor(p.getValor());
+				
+				em.getTransaction().commit();
+			} catch(RuntimeException e) {
+				em.getTransaction().rollback();
+			} finally {
+				em.close();
+			}
+		}
+		
+		public void excluir(int codigo) {
+			EntityManager em = JPAResourceBean.getEntityManager();
+			
+			try {
+				em.getTransaction().begin();
+				
+				Produto p = em.find(Produto.class, codigo);
+				em.remove(p);
+				
+				em.getTransaction().commit();
+			} catch(RuntimeException e) {
+				em.getTransaction().rollback();
+			} finally {
+				em.close();
+			}
+		}
+		
+		public Produto buscar(int codigo) {
+			EntityManager em = JPAResourceBean.getEntityManager();
+			return em.find(Produto.class, codigo);
+		}
+		
+		public List<Produto> listar() {
+			EntityManager em = JPAResourceBean.getEntityManager();
+			
+			return em.createQuery("select p from Produto p").getResultList();
+		}
 }
