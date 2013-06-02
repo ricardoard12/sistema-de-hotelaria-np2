890999fc8876d61f1623deabfd45b9df02cad67c
nmacambira <nmacambira@gmail.com>
2013-06-02 01:44:01 +0000

    Bom. Fiz todos os Daos e já comecei a fazer as telas de login, funcionario, cliente e erro. Estou no momento testando a validacao de login. A pagina login.xhtml não funciona no trabalho compartilhado mas funciona no meu local.... vai entender...

--- Sistema de Hotelaria/src/hotel/formularios/UsuarioForm.java
+++ Sistema de Hotelaria/src/hotel/formularios/UsuarioForm.java
@@ -1,20 +1,47 @@
 package hotel.formularios;
 
+import java.util.List;
+
 import hotel.entidades.Usuario;
 import hotel.persistencia.UsuarioDao;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
 
 @ManagedBean
 @ViewScoped
 public class UsuarioForm {
 	private Usuario usuario = new Usuario();
-	
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public String autenticarUser() {
+		UsuarioDao uDao = new UsuarioDao();
+		List<Usuario> usuLista = uDao.listar();
+
+		for (Usuario usu : usuLista) {
+			if (usuario.getLogin().equals(usu.getLogin())
+					&& usuario.getSenha().equals(usu.getSenha())) {
+				if (usu.getPerfil().equals("funcionario")) {
+					return "funcionario";
+				} else {
+					return "cliente";
+				}
+			}		
+		}
+		return "erro";
+	}
+
 	public String inserir() {
 		UsuarioDao uDao = new UsuarioDao();
 		uDao.insertNovoUsuario(usuario);
-		
 		return "sucesso";
 	}
 }
